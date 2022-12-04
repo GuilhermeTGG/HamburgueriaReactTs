6227a82820b51f2b9f4902dfe7628a551884d969
GuilhermeTGG <guilherme050592@gmail.com>
2022-12-04 11:02:52 -0300

    Hamburguer

---
+++ src/components/Menu/styles.ts
@@ -0,0 +1,75 @@
+import styled, { css } from "styled-components";
+
+interface MenuItemProps {
+  logout?: boolean;
+  active?: boolean;
+}
+
+interface MenuItemButtonProps {
+  active?: boolean;
+}
+
+export const MenuContainer = styled.div`
+  width: 6.5rem;
+  height: 100vh;
+  border-radius: 16px;
+  background-color: #1f1d2b;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  img {
+    width: 4rem;
+    margin: 1.25rem 0;
+  }
+  nav{
+    width 100%;
+  }
+`;
+
+export const MenuItem = styled.div`
+  height: 80px;
+  width: calc(100% - 12px);
+  margin-left: 12px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  border-radius: 12px 0px 0px 12px;
+  background-color: #1f1d2b;
+
+  ${({ logout }: MenuItemProps) =>
+    logout &&
+    css`
+      position: absolute;
+      bottom: 0;
+      width: calc(6.5rem - 12px);
+      border-radius: 16px;
+    `}
+
+  ${({ active }: MenuItemProps) =>
+    active &&
+    css`
+      background-color: #252836;
+    `}
+`;
+
+export const MenuItemButton = styled.button`
+  width: 56px;
+  height: 56px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  margin-left: 12px;
+  background-color: #1f1d2b;
+  border-radius: 8px;
+  color: #6cea69;
+  cursor: pointer;
+
+  :hover {
+    transform: scale(1.2);
+  }
+
+  ${({ active }: MenuItemButtonProps) => active && css`
+  background-color: #6cea69;
+  color: #ffffff;
+  `}
+`;
---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,73 @@
+import Menu from "../../components/Menu";
+import * as Styled from "./styles";
+
+const Home = () => {
+  return (
+    <Styled.HomeContainer>
+      <Menu path="home" />
+      <div className="home-content">
+        <header>
+          <div>
+            <h1>Burguer Fresh</h1>
+            <p>{Date.now()}</p>
+          </div>
+          <div>
+            <img alt="search-icon" />
+          </div>
+        </header>
+        <section>
+          <div>
+            <p>Lanches</p>
+          </div>
+          <div>
+            <h2>Ecolha seu Lanche</h2>
+            <select>
+              <option value="1">1</option>
+            </select>
+          </div>
+          <div className="list">
+            <div>Card</div>
+            <div>Card</div>
+            <div>Card</div>
+            <div>Card</div>
+            <div>Card</div>
+          </div>
+        </section>
+      </div>
+      <aside>
+        <header>
+          <h2>Pedido 12</h2>
+          <div>
+            <button>Comer no Local</button>
+            <button>P/ Viagem</button>
+            <button>Delivery</button>
+          </div>
+        </header>
+        <div>
+          <div>
+            <h3>Item</h3>
+            <h3>Qtd</h3>
+            <h3>Preço</h3>
+          </div>
+          <div className="checkout-cards-container">
+            <div>Card Checkout</div>
+            <div>Card Checkout</div>
+            <div>Card Checkout</div>
+          </div>
+        </div>
+        <div>
+          <div>
+            <p>Desconto</p>
+            <p>R$0.00</p>
+          </div>
+          <div>
+            <p>Sub total</p>
+            <p>R$0.00</p>
+          </div>
+          <button>Continuar para o pagamento</button>
+        </div>
+      </aside>
+      </Styled.HomeContainer>
+  );
+};
+export default Home;
